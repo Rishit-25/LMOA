ca2015c1c5d208baae4f6e1f6df924c787c772ef
Rishit-25 <123052426+Rishit-25@users.noreply.github.com>
2023-08-17 04:42:23 +0000

    sw

---
+++ hotel managenent.py/login_check.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("HOTEL MANAGEMENT.db")
+cursor = conn.cursor()
+
+cursor.execute ('''SELECT * FROM login_table''')
+
+r= cursor.fetchall()
+
+def login_check(email,password):
+    global r
+    for i in r :
+        if i[0] == email and i[1] == password :
+            return True
+        else:
+            return False 
+
---
+++ hotel managenent.py/test.py
@@ -0,0 +1,9 @@
+i=[]
+
+a=input("Enter the  name  :  " )
+b=int(input("phone number :  "))
+c=input("enteer your gmail : ")
+d=input("Enter teh chenck in date :  ")
+e=input("Enetr the checkout date :  ")
+i.append(a,b,c,d,e)
+print(i)
