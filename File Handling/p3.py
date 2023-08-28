aed3fd88198a586e8822a049547955bb1796b8cd
Rishit-25 <123052426+Rishit-25@users.noreply.github.com>
2023-08-28 04:45:26 +0000

    do it

--- File Handling/p3.py
+++ File Handling/p3.py
@@ -1,4 +1,9 @@
 myfile = open("Otherfile.dat", "r+")
 content = myfile.readlines()
-for line in myfile:
-    print(line)
+i=0
+for line in content:
+    for j in line:
+        if j != " " and j!="\n" :
+            i= i +1
+
+print(i)
