dfbc9db01ec2d0313f94a0ddc14e01e779e8741b
Rishit-25 <123052426+Rishit-25@users.noreply.github.com>
2023-10-25 04:37:03 +0000

    do it

--- Hotel management backup/test.py
+++ Hotel management backup/test.py
@@ -1,3 +1,15 @@
-cursor.execute("SELECT * FROM Booking_table WHERE Room_no = ?" , (r[i][0]))
-        b= cursor.fetchall()
-        print(b)
+Topic : Python with SQL
+Question: The books table of test database contains the record shown below
+
+Title ISBN
+Die to Live 78127873915
+Again 23686286243
+Ushakaal 12678987036
+Ushakiran’ 42568987036
+
+Question: Write a python database connectivity script that
+(i) displays one record at a time from the above table
+(ii) Inserts a new row to the table through python code
+(iii) Change Title of book Again to ‘New Beginning’
+(iv) Count the total number of records after the new insertion
+Display screen shots of the SQL interface and python outputs
